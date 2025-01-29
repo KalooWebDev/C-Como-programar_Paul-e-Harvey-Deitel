195fc7593fd813aab4cbb5bb6d7fa6e4a4764ba0
Renan Calu <renan.calu@psi.local>
2025-01-29 19:28:39 -0300

    Exercício resolvido (Imprimindo valores com printf).

---
+++ Exercicios_Capitulo2/Imprimindo_valores.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 1, b = 2, c = 3, d = 4;
+
+    printf("1, 2, 3, 4\n");
+    printf("%d, %d, %d, %d\n", a, b, c, d);
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    printf("%d\n", d);
+
+    return 0;
+}
