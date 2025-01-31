c5f41b509ec49c29ed1799a3c15a04fd6dfc2feb
Renan Calu <renan.calu@psi.local>
2025-01-31 16:28:30 -0300

    Exercício resolvido (Tabelas de quadrados e cubos).

---
+++ Exercicios_Capitulo2/Tabelas_de_quadrados_e_cubos.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+
+int main(void){
+    int numero = 0;
+    int quadrado;
+    int cubo;
+
+    printf("Numero\tQuadrado\tCubo\n");
+
+    numero = numero;
+    printf("%d\t", numero);
+
+    numero = numero;
+    quadrado = numero * numero;
+    printf("%d\t\t", quadrado);
+
+    numero = numero;
+    cubo = numero * numero * numero;
+    printf("%d\n", cubo);
+
+    numero = 1;
+    printf("%d\t", numero);
+
+    numero = 1;
+    quadrado = numero * numero;
+    printf("%d\t\t", quadrado);
+
+    numero = 1;
+    cubo = numero * numero * numero;
+    printf("%d\n", cubo);
+
+    numero = 2;
+    printf("%d\t", numero);
+
+    numero = 2;
+    quadrado = numero * numero;
+    printf("%d\t\t", quadrado);
+
+    numero = 2;
+    cubo = numero * numero * numero;
+    printf("%d\n", cubo);
+
+    numero = 3;
+    printf("%d\t", numero);
+
+    numero = 3;
+    quadrado = numero * numero;
+    printf("%d\t\t", quadrado);
+
+    numero = 3;
+    cubo = numero * numero * numero;
+    printf("%d\n", cubo);
+
+    numero = 4;
+    printf("%d\t", numero);
+
+    numero = 4;
+    quadrado = numero * numero;
+    printf("%d\t\t", quadrado);
+
+    numero = 4;
+    cubo = numero * numero * numero;
+    printf("%d\n", cubo);
+
+    numero = 5;
+    printf("%d\t", numero);
+
+    numero = 5;
+    quadrado = numero * numero;
+    printf("%d\t\t", quadrado);
+
+    numero = 5;
+    cubo = numero * numero * numero;
+    printf("%d\n", cubo);
+
+    numero = 6;
+    printf("%d\t", numero);
+
+    numero = 6;
+    quadrado = numero * numero;
+    printf("%d\t\t", quadrado);
+
+    numero = 6;
+    cubo = numero * numero * numero;
+    printf("%d\n", cubo);
+
+    numero = 7;
+    printf("%d\t", numero);
+
+    numero = 7;
+    quadrado = numero * numero;
+    printf("%d\t\t", quadrado);
+
+    numero = 7;
+    cubo = numero * numero * numero;
+    printf("%d\n", cubo);
+
+    numero = 8;
+    printf("%d\t", numero);
+
+    numero = 8;
+    quadrado = numero * numero;
+    printf("%d\t\t", quadrado);
+
+    numero = 8;
+    cubo = numero * numero * numero;
+    printf("%d\n", cubo);
+
+    numero = 9;
+    printf("%d\t", numero);
+
+    numero = 9;
+    quadrado = numero * numero;
+    printf("%d\t\t", quadrado);
+
+    numero = 9;
+    cubo = numero * numero * numero;
+    printf("%d\n", cubo);
+
+    numero = 10;
+    printf("%d\t", numero);
+
+    numero = 10;
+    quadrado = numero * numero;
+    printf("%d\t\t", quadrado);
+
+    numero = 10;
+    cubo = numero * numero * numero;
+    printf("%d\n", cubo);
+
+    return 0;
+}
