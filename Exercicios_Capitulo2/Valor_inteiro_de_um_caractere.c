9b7b23ec5e08c1187cb6f2e1611434ab06f670de
Renan Calu <renan.calu@psi.local>
2025-01-31 16:07:30 -0300

    Exercício resolvido (Valor inteiro de um caractere).

---
+++ Exercicios_Capitulo2/Valor_inteiro_de_um_caractere.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n", 'A', 'B', 'C', 'a', 'b', 'c', '0');
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n", '1', '2', '$', '*', '+', '/', ' ');
+
+    return 0;
+}
